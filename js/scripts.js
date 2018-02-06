b63175c7133c7f1bf42e36360cbf2f049e00bd2d
mtallerico1 <mtallerico1@gmail.com>
2018-02-05 21:06:44 -0500

    search funtion added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-function searchImages() {
-    
+let searchInput = document.getElementById('mySearch').value;
+let captions = document.querySelectorAll('figcaption');
+let textWords;
+
+// let figureText = captions.textContent;
+
+function searchImage(input) {
+    for (i=0; i<=captions.length; i++) {
+        figureText = captions[i].textContent;
+        // figureText = textWords.textContent;
+        if (figureText.search() === searchInput) {
+            console.log(yeah);
+        }
+    };
 }
 
