641e5793fe2d6e6f8a668f5e590974f0a26e0473
mtallerico1 <mtallerico1@gmail.com>
2018-02-07 05:58:20 -0500

    Fixed some issues with files paths

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ searchBox.addEventListener("keyup", searchImage);
 
 function searchImage() {
     for (var i=0; i<captions.length; i++) {
-        figureText = captions[i].textContent.toLowerCase();
-        searchInput = searchBox.value.toLowerCase();
-        doesContain = figureText.search(searchInput);
-        parentEl = captions[i].parentElement.parentElement;
+        let figureText = captions[i].textContent.toLowerCase();
+        let searchInput = searchBox.value.toLowerCase();
+        let doesContain = figureText.search(searchInput);
+        let parentEl = captions[i].parentElement.parentElement;
         if (doesContain === -1) {
             parentEl.classList.add("hideImage");
         } else if (doesContain != -1) {
             parentEl.classList.remove("hideImage");
         }
     }
-};
+}
 
